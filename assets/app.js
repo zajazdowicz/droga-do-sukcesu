d24a1bb400ae65e1010b3f6866128e28cb198dc2
LS <lukasz@shc.digital>
2023-03-02 18:16:23 +0100

    react-test

---
+++ assets/app.js
@@ -0,0 +1,14 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './css/app.css';
+import Home from './components/Home';
+    
+ReactDOM.render(<Router><Home /></Router>, document.getElementById('root'));

7db3b6421587b6fc114a30f881b1b6f708facef3
LS <lukasz@shc.digital>
2023-03-02 21:16:10 +0100

    aa

--- assets/components/Home.js
+++ assets/components/Home.js
@@ -11,34 +11,12 @@ class Home extends Component {
           humor: 'happy'
         }
       }
-    
-    
+
     render() {
-       // return <h1>Co za szajs, {this.props.name}</h1>;
         return (
-            
+
            <div>
                 <h1>Co za szajs, {this.props.name}</h1>
-
-               <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                   {/* <Link className={"navbar-brand"} to={"/"}> Symfony React Project </Link> */}
-                   <div className="collapse navbar-collapse" id="navbarText">
-                       <ul className="navbar-nav mr-auto">
-                           <li className="nav-item">
-                               {/* <Link className={"nav-link"} to={"/posts"}> Posts </Link> */}
-                           </li>
-    
-                           <li className="nav-item">
-                               {/* <Link className={"nav-link"} to={"/users"}> Users </Link> */}
-                           </li>
-                       </ul>
-                   </div>
-               </nav>
-               {/* <Switch>
-                   <Redirect exact from="/" to="/users" />
-                   <Route path="/users" component={Users} />
-                   <Route path="/posts" component={Posts} />
-               </Switch> */}
            </div>
         )
     }

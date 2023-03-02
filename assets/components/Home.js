7be98c50843770a29c3b47711b00e86dd3de864e
LS <lukasz@shc.digital>
2023-03-02 20:40:22 +0100

    kjl

--- assets/components/Home.js
+++ assets/components/Home.js
@@ -6,30 +6,33 @@ class Home extends Component {
     
     
     render() {
-        return <h1>Co za szajs, {this.props.name}</h1>;
-        // return (
-        //    <div>
-        //        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        //            <Link className={"navbar-brand"} to={"/"}> Symfony React Project </Link>
-        //            <div className="collapse navbar-collapse" id="navbarText">
-        //                <ul className="navbar-nav mr-auto">
-        //                    <li className="nav-item">
-        //                        <Link className={"nav-link"} to={"/posts"}> Posts </Link>
-        //                    </li>
+       // return <h1>Co za szajs, {this.props.name}</h1>;
+        return (
+            
+           <div>
+                <h1>Co za szajs, {this.props.name}</h1>
+
+               <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                   {/* <Link className={"navbar-brand"} to={"/"}> Symfony React Project </Link> */}
+                   <div className="collapse navbar-collapse" id="navbarText">
+                       <ul className="navbar-nav mr-auto">
+                           <li className="nav-item">
+                               {/* <Link className={"nav-link"} to={"/posts"}> Posts </Link> */}
+                           </li>
     
-        //                    <li className="nav-item">
-        //                        <Link className={"nav-link"} to={"/users"}> Users </Link>
-        //                    </li>
-        //                </ul>
-        //            </div>
-        //        </nav>
-        //        <Switch>
-        //            <Redirect exact from="/" to="/users" />
-        //            <Route path="/users" component={Users} />
-        //            <Route path="/posts" component={Posts} />
-        //        </Switch>
-        //    </div>
-        // )
+                           <li className="nav-item">
+                               {/* <Link className={"nav-link"} to={"/users"}> Users </Link> */}
+                           </li>
+                       </ul>
+                   </div>
+               </nav>
+               {/* <Switch>
+                   <Redirect exact from="/" to="/users" />
+                   <Route path="/users" component={Users} />
+                   <Route path="/posts" component={Posts} />
+               </Switch> */}
+           </div>
+        )
     }
 
 
